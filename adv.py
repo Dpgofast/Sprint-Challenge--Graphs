933f01b32ddcc51b0fff68d4e61071daaf58b299
Dpgofast <43558343+Dpgofast@users.noreply.github.com>
2020-01-24 13:47:33 -0600

    Commentary complete

--- adv.py
+++ adv.py
@@ -103,7 +103,7 @@ def spelunk(player, moves_cue):
                 if graph[last_room][exit] == "?":
                     # add path to list to explore
                     return path
-                    # otherwise remove path as already explored
+                    # otherwise path is already explored
                 else:
                     lost = list(path)
                     lost.append(graph[last_room][exit])
@@ -112,58 +112,99 @@ def spelunk(player, moves_cue):
 
 
 def cue_moves(player, moves_cue):
+    # current exits for the room currently in
     current_exits = graph[player.current_room.id]
+    # track exits we havent tried
     untried_exits = []
+    # for every valid direction from where we are
     for direction in current_exits:
+        # If we find one unexplored path
         if current_exits[direction] == "?":
+            # put it on the list to check out
             untried_exits.append(direction)
+    # if we run out of unknowns
     if len(untried_exits) == 0:
+        # list unknown paths to explore
         unexplored = spelunk(player, moves_cue)
+        # track where we are
         room_num = player.current_room.id
+        # go through list of unexplored paths
         for next in unexplored:
+            # go through all possible moves in each room
             for direction in graph[room_num]:
+                # if the room has an unexplored path available
                 if graph[room_num][direction] == next:
+                    # explore and add to the queue of moves
                     moves_cue.enqueue(direction)
+                    # Exhaust all options
                     room_num = next
                     break
+    # otherwise start down a random path
     else:
         moves_cue.enqueue(untried_exits[random.randint(0, len(untried_exits) - 1)])
 
 
+# set iteration limit
 tries = 100
+# Track our best score 997 was my best in previous iteration
 optimum_len = 997
+# Track the optimum path to take
 optimum_path = []
+# run until max max tries
 for x in range(tries):
+    # initialize player in the world
     player = Player(world.starting_room)
+    # initialize graph
     graph = {}
-
+    # start in a new room
     fresh_room = {}
+    # check the possible directions/exits in the room
     for direction in player.current_room.get_exits():
+        # mark all exits as unexplored
         fresh_room[direction] = "?"
+    # add the new room and exits to the graph at the starting room id index
     graph[world.starting_room.id] = fresh_room
-
+    # Initialize a queue for the moves
     moves_cue = Queue()
+    # keep track of moves in a list
     total_moves = []
+    # start exploring
     cue_moves(player, moves_cue)
-
+    #change directions to go backward through
     reverse_compass = {"n": "s", "s": "n", "e": "w", "w": "e"}
-
+    # while possible moves/unexplored areas
     while moves_cue.size() > 0:
+        # track starting room
         starting = player.current_room.id
+        # get next path from queue
         next = moves_cue.dequeue()
+        # explore path
         player.travel(next)
+        # add to list of moves
         total_moves.append(next)
+        # track where you end up
         end = player.current_room.id
+        # add to graph connections
         graph[starting][next] = end
+        # if connection not there
         if end not in graph:
+            # set it
             graph[end] = {}
+            # for each exit in possible exits from location
             for exit in player.current_room.get_exits():
+                # mark as unexplored
                 graph[end][exit] = "?"
+        # go back from whence you came
         graph[end][reverse_compass[next]] = starting
+        # if you run out of moves in queue
         if moves_cue.size() == 0:
+            # check again and explore
             cue_moves(player, moves_cue)
+    # if we beat our previous score
     if len(total_moves) < optimum_len:
-        optimum_path= total_moves
+        # our best path is the one we just explored
+        optimum_path = total_moves
+        # best move count is our last iteration
         optimum_len = len(total_moves)
 
 # !!!! TESTS PASSED: 974 moves, 500 rooms visited !!!
